f268be40dcad4867be0dfe8dafc133c46ebedee5
Megasus <153267993+Megasus69@users.noreply.github.com>
2024-12-13 09:54:44 +0800

    ...

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
-=======
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
->>>>>>> b9f39ad26e4b0eb49b869c1c2fe824d9f69dea0d
